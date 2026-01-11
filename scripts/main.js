9bb006ccbc70d8c5abcf2e08cae87a2986a86bdc
Albert Kemp <albertkemp@3360L-144597-M.local>
2026-01-12 00:37:28 +1100

    Initial test of photo website

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+const elements = document.querySelectorAll("#Melbourne", "#Bermagui", "#Botanics", "#Metung", "#PortFairy", "#Qld", "#WTP", "#Misc", "#Contact");
+const [Melbourne, Bermagui, Botanics, Metung, PortFairy, Qld, WTP, Misc, Contact] = elements;
+
+let pageData;
+
+fetch('data/index.json')
+  .then(response => response.json())
+  .then(data => {
+      pageData = data;
+  })
+  .catch(error => {
+    console.error('Error fetching the JSON file:', error);
+  });
+
+elements.forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    const folder_name = pageData[e.target.id].folder_name;
+    const folder_number = pageData[e.target.id].folder_number;
+    const container = document.body.querySelector("#container");
+    container.innerHTML = "";
+    for (i = 0; i < folder_number; i++) {
+        container.append(`<img src="images/${folder_name}/img${i}.JPG" height="200" title="&copy; Albert Kemp">`);
+    }
+  });
+});
